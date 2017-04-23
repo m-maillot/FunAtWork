73f6f5685b9b29a854b5b46603ef9492b63d17e9
Martial Maillot <mmaillot@v3d.fr>
2017-04-23 21:41:07 +0200

    Define default timezone if not already define in php.ini

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
+// insert here the default timezone
+date_default_timezone_set('Europe/Paris');
+
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
-    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $url = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
     if (is_file($file)) {
         return false;
