31123dc6f6443292306f4a10e6cfaaeb8d3e8e41
Martial Maillot <mmaillot@v3d.fr>
2017-09-05 13:05:53 +0200

    Should use user auth for each call to route to right group

--- src/routes.php
+++ src/routes.php
@@ -16,18 +16,18 @@ $app->group('/api/v1', function () use ($app) {
     $this->map(['POST'], '/signin', "App\Action\PlayerAction:signin");
     // Player API
     $this->map(['POST'], '/players', 'App\Action\PlayerAction:create')->add(new TokenAuth($app, TokenAuth::SCOPE_ADMIN));
-    $this->map(['GET'], '/players', 'App\Action\PlayerAction:fetch');
-    $this->map(['GET'], '/players/{player_id}', 'App\Action\PlayerAction:fetchOne');
+    $this->map(['GET'], '/players', 'App\Action\PlayerAction:fetch')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
+    $this->map(['GET'], '/players/{player_id}', 'App\Action\PlayerAction:fetchOne')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
 
     // Babyfoot API
     $this->group('/babyfoot', function () use ($app) {
-        $this->map(['GET'], '/games', 'App\Action\BabyfootAction:fetchGames');
+        $this->map(['GET'], '/games', 'App\Action\BabyfootAction:fetchGames')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
         $this->map(['POST'], '/start', 'App\Action\BabyfootAction:startGame')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
-        $this->map(['GET'], '/games/{game_id}', 'App\Action\BabyfootAction:fetchOneGame');
+        $this->map(['GET'], '/games/{game_id}', 'App\Action\BabyfootAction:fetchOneGame')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
         $this->map(['POST'], '/stop', 'App\Action\BabyfootAction:gameOver')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
         $this->map(['POST'], '/goal', 'App\Action\BabyfootAction:addGoal')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
-        $this->map(['GET'], '/stats/team', 'App\Action\BabyfootAction:computeTeamStats');
-        $this->map(['GET'], '/stats/player', 'App\Action\BabyfootAction:computePlayerStats');
-        $this->map(['GET'], '/stats/player/{player_id}', 'App\Action\BabyfootAction:computeUniquePlayerStats');
+        $this->map(['GET'], '/stats/team', 'App\Action\BabyfootAction:computeTeamStats')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
+        $this->map(['GET'], '/stats/player', 'App\Action\BabyfootAction:computePlayerStats')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
+        $this->map(['GET'], '/stats/player/{player_id}', 'App\Action\BabyfootAction:computeUniquePlayerStats')->add(new TokenAuth($app, TokenAuth::SCOPE_LOGGED));
     });
 });
